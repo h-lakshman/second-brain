5d5c317a578daa20949554629a1c2960c249b8c1
h-lakshman <lakshman.vaibhav007@gmail.com>
2025-03-01 21:41:27 +0530

    setup axios for sending req to backend

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/api/v1";
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const signin = (username: string, password: string) => {
+  return api.post("/signin", { username, password });
+};
